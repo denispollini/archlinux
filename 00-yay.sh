e6d31e9dc29f40d7a2ec947528cdb7a0b0beaca2
Denis Pollini <denis.pollini.90@gmail.com>
2026-03-07 15:47:22 +0100

    add yay.sh

---
+++ 00-yay.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo
+tput setaf 3
+echo "######################################################"
+echo "###################  Install the prerequisite packages"
+echo "######################################################"
+tput sgr0
+echo
+#  Install the prerequisite packages
+sudo pacman -Syu
+sudo pacman -S --needed --noconfirm base-devel git
+
+echo
+tput setaf 3
+echo "######################################################"
+echo "################### Clone the Yay git repo and switch to it"
+echo "######################################################"
+tput sgr0
+echo
+# Clone the Yay git repo and switch to it
+git clone https://aur.archlinux.org/yay.git
+cd yay
+
+echo
+tput setaf 3
+echo "######################################################"
+echo "################### Install yay"
+echo "######################################################"
+tput sgr0
+echo
+# Install yay
+makepkg -si
