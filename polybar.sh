197bc4414dd5fa39c38a82ae2159d5b81530c643
Denis Pollini <denis.pollini.90@gmail.com>
2026-03-08 12:21:50 +0100

    edit polybar

--- polybar.sh
+++ polybar.sh
@@ -33,20 +33,132 @@ if [[ $(echo $XDG_SESSION_DESKTOP) == "i3" ]]; then
 echo -e "exec_always --no-startup-id $HOME/.config/polybar/launch.sh" >> $HOME/.config/i3/config
 fi
 
+echo
+tput setaf 3
+echo "######################################################"
+echo "################### Create Configuration File and Launch Script"
+echo "######################################################"
+tput sgr0
+echo
+# Create Configuration File and Launch Script
 mkdir -p $HOME/.config/polybar
+
+cat <<EOF > $HOME/.config/polybar/config.ini
+[colors]
+background = #000000
+foreground = #FFFFFF
+foreground-alt = #000000
+secondary = #8ABEB7
+alert = #A54242
+disabled = #707880
+clown-foreground = #ffffff
+primary = #121315
+background-alt = #ffffff
+
+[bar/bar1]
+width = 100%
+height = 18
+radius = 2
+dpi = 0
+background = ${colors.background}
+foreground = ${colors.foreground}
+line-size = 0
+border-size = 4pt
+border-color = #00000000
+padding-left = 0
+padding-right = 1
+module-margin = 1
+separator = |
+separator-foreground = ${colors.disabled}
+font-0 = "MartianMono Nerd Font Propo,MartianMono NFP,MartianMono NFP Cond Med:style=Condensed Medium,Regular:size=8:pixelsize=10;2"
+modules-left = xworkspaces xwindow
+modules-right = filesystem memory cpu net date
+cursor-click = pointer
+cursor-scroll = ns-resize
+enable-ipc = true
+
+[module/xworkspaces]
+type = internal/xworkspaces
+label-active = %name%
+label-active-background = ${colors.background-alt}
+label-active-foreground = ${colors.foreground-alt}
+label-active-underline= ${colors.primary}
+label-active-padding = 1
+label-occupied = %name%
+label-occupied-padding = 1
+label-urgent = %name%
+label-urgent-background = ${colors.alert}
+label-urgent-padding = 1
+label-empty = %name%
+label-empty-foreground = ${colors.disabled}
+label-empty-padding = 1
+
+[module/xwindow]
+type = internal/xwindow
+label = %{F#bbbbbc}changeme
+
+[module/filesystem]
+type = internal/fs
+interval = 25
+mount-0 = /
+label-mounted = %{F#bbbbbc}%mountpoint%%{F-} %percentage_used%%
+label-unmounted = %mountpoint% not mounted
+label-unmounted-foreground = ${colors.disabled}
+label-indicator-padding = 2
+label-indicator-margin = 1
+label-indicator-foreground = ${colors.background}
+label-indicator-background = ${colors.secondary}
+
+[module/memory]
+type = internal/memory
+interval = 2
+format-prefix = "RAM "
+format-prefix-foreground = ${colors.clown-foreground}
+label = %percentage_used:2%%
+
+[module/cpu]
+type = internal/cpu
+interval = 2
+format-prefix = "CPU "
+format-prefix-foreground = ${colors.clown-foreground}
+label = %percentage:2%%
+
+[module/net]
+type = internal/network
+interval = 5
+format-connected = <label-connected>
+format-disconnected = <label-disconnected>
+label-disconnected = %{F#99a09b}changeme_network_interface%{F#ff2300} disconnected
+label-connected = %{F#99a09b}changeme_network_interface%{F#707880} %local_ip% %upspeed% %downspeed%
+interface-type = wired
+speed-unit = b/s
+
+[module/date]
+type = internal/date
+interval = 1
+date = %H:%M
+date-alt = %Y-%m-%d %H:%M:%S
+label = %date%
+label-foreground = ${colors.clown-foreground}
+
+[settings]
+screenchange-reload = true
+pseudo-transparency = true
+EOF
 
 cat <<EOF > $HOME/.config/polybar/launch.sh
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar
-echo "---" | tee -a /tmp/polybar.log 
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
+# Launch bar1
+echo "---" | tee -a /tmp/polybar.log
+polybar bar1 -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log &
+disown
 
 echo "Bars launched..."
 EOF
