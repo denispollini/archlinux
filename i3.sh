2492c3d6fda7dbcb6301a4c176b5a2bf7c9b61c3
Denis Pollini <denis.pollini.90@gmail.com>
2026-03-08 11:39:29 +0100

    edit i3

--- i3.sh
+++ i3.sh
@@ -10,3 +10,6 @@ tput sgr0
 echo
 # Install i3
 sudo pacman -S --noconfirm --needed i3-wm
+
+if [[ $(systemd-detect-virt) == "vmware" ]]; then
+echo -e "exec --no-startup-id /usr/bin/vmware-user" >> $HOME/.config/i3/config
