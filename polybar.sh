184ba64f6332290c641d8a8ef37bda14156adc03
Denis Pollini <denis.pollini.90@gmail.com>
2026-03-09 21:55:25 +0100

    edit polybar

--- polybar.sh
+++ polybar.sh
@@ -45,66 +45,139 @@ echo
 mkdir -p $HOME/.config/polybar
 
 cat <<EOF > $HOME/.config/polybar/config.ini
+;==========================================================
+;
+;
+;   ██████╗  ██████╗ ██╗  ██╗   ██╗██████╗  █████╗ ██████╗
+;   ██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝██╔══██╗██╔══██╗██╔══██╗
+;   ██████╔╝██║   ██║██║   ╚████╔╝ ██████╔╝███████║██████╔╝
+;   ██╔═══╝ ██║   ██║██║    ╚██╔╝  ██╔══██╗██╔══██║██╔══██╗
+;   ██║     ╚██████╔╝███████╗██║   ██████╔╝██║  ██║██║  ██║
+;   ╚═╝      ╚═════╝ ╚══════╝╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+;
+;
+;   To learn more about how to configure Polybar
+;   go to https://github.com/polybar/polybar
+;
+;   The README contains a lot of information
+;
+;==========================================================
+
 [colors]
-background = #000000
-foreground = #FFFFFF
-foreground-alt = #000000
+background = #282A2E
+background-alt = #373B41
+foreground = #C5C8C6
+primary = #F0C674
 secondary = #8ABEB7
 alert = #A54242
 disabled = #707880
-clown-foreground = #ffffff
-primary = #121315
-background-alt = #ffffff
 
-[bar/bar1]
+[bar/example]
 width = 100%
-height = 18
-radius = 2
-dpi = 0
+height = 24pt
+radius = 6
+
+; dpi = 96
+
 background = ${colors.background}
 foreground = ${colors.foreground}
-line-size = 0
+
+line-size = 3pt
+
 border-size = 4pt
 border-color = #00000000
+
 padding-left = 0
 padding-right = 1
+
 module-margin = 1
+
 separator = |
 separator-foreground = ${colors.disabled}
-font-0 = "MartianMono Nerd Font Propo,MartianMono NFP,MartianMono NFP Cond Med:style=Condensed Medium,Regular:size=8:pixelsize=10;2"
+
+font-0 = monospace;2
+
 modules-left = xworkspaces xwindow
-modules-right = filesystem memory cpu net date
+modules-right = filesystem pulseaudio xkeyboard memory cpu wlan eth date
+
 cursor-click = pointer
 cursor-scroll = ns-resize
+
 enable-ipc = true
+
+; wm-restack = generic
+; wm-restack = bspwm
+; wm-restack = i3
+
+; override-redirect = true
+
+; This module is not active by default (to enable it, add it to one of the
+; modules-* list above).
+; Please note that only a single tray can exist at any time. If you launch
+; multiple bars with this module, only a single one will show it, the others
+; will produce a warning. Which bar gets the module is timing dependent and can
+; be quite random.
+; For more information, see the documentation page for this module:
+; https://polybar.readthedocs.io/en/stable/user/modules/tray.html
+[module/systray]
+type = internal/tray
+
+format-margin = 8pt
+tray-spacing = 16pt
 
 [module/xworkspaces]
 type = internal/xworkspaces
+
 label-active = %name%
 label-active-background = ${colors.background-alt}
-label-active-foreground = ${colors.foreground-alt}
 label-active-underline= ${colors.primary}
 label-active-padding = 1
+
 label-occupied = %name%
 label-occupied-padding = 1
+
 label-urgent = %name%
 label-urgent-background = ${colors.alert}
 label-urgent-padding = 1
+
 label-empty = %name%
 label-empty-foreground = ${colors.disabled}
 label-empty-padding = 1
 
 [module/xwindow]
 type = internal/xwindow
-label = %{F#bbbbbc}changeme
+label = %title:0:60:...%
 
 [module/filesystem]
 type = internal/fs
 interval = 25
+
 mount-0 = /
-label-mounted = %{F#bbbbbc}%mountpoint%%{F-} %percentage_used%%
+
+label-mounted = %{F#F0C674}%mountpoint%%{F-} %percentage_used%%
+
 label-unmounted = %mountpoint% not mounted
 label-unmounted-foreground = ${colors.disabled}
+
+[module/pulseaudio]
+type = internal/pulseaudio
+
+format-volume-prefix = "VOL "
+format-volume-prefix-foreground = ${colors.primary}
+format-volume = <label-volume>
+
+label-volume = %percentage%%
+
+label-muted = muted
+label-muted-foreground = ${colors.disabled}
+
+[module/xkeyboard]
+type = internal/xkeyboard
+blacklist-0 = num lock
+
+label-layout = %layout%
+label-layout-foreground = ${colors.primary}
+
 label-indicator-padding = 2
 label-indicator-margin = 1
 label-indicator-foreground = ${colors.background}
@@ -114,54 +187,63 @@ label-indicator-background = ${colors.secondary}
 type = internal/memory
 interval = 2
 format-prefix = "RAM "
-format-prefix-foreground = ${colors.clown-foreground}
+format-prefix-foreground = ${colors.primary}
 label = %percentage_used:2%%
 
 [module/cpu]
 type = internal/cpu
 interval = 2
 format-prefix = "CPU "
-format-prefix-foreground = ${colors.clown-foreground}
+format-prefix-foreground = ${colors.primary}
 label = %percentage:2%%
 
-[module/net]
+[network-base]
 type = internal/network
 interval = 5
 format-connected = <label-connected>
 format-disconnected = <label-disconnected>
-label-disconnected = %{F#99a09b}changeme_network_interface%{F#ff2300} disconnected
-label-connected = %{F#99a09b}changeme_network_interface%{F#707880} %local_ip% %upspeed% %downspeed%
+label-disconnected = %{F#F0C674}%ifname%%{F#707880} disconnected
+
+[module/wlan]
+inherit = network-base
+interface-type = wireless
+label-connected = %{F#F0C674}%ifname%%{F-} %essid% %local_ip%
+
+[module/eth]
+inherit = network-base
 interface-type = wired
-speed-unit = b/s
+label-connected = %{F#F0C674}%ifname%%{F-} %local_ip%
 
 [module/date]
 type = internal/date
 interval = 1
+
 date = %H:%M
 date-alt = %Y-%m-%d %H:%M:%S
+
 label = %date%
-label-foreground = ${colors.clown-foreground}
+label-foreground = ${colors.primary}
 
 [settings]
 screenchange-reload = true
 pseudo-transparency = true
 EOF
 
-#cat <<EOF > $HOME/.config/polybar/launch.sh
-##!/usr/bin/env bash
-#
-## Terminate already running bar instances
-## If all your bars have ipc enabled, you can use
-#polybar-msg cmd quit
-## Otherwise you can use the nuclear option:
-## killall -q polybar
-#
-## Launch bar1
-#echo "---" | tee -a /tmp/polybar.log
-#polybar bar1 -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log &
-#disown
-#
-#echo "Bars launched..."
-#EOF
-#
-#chmod +x $HOME/.config/polybar/launch.sh
+cat <<EOF > $HOME/.config/polybar/launch.sh
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1
+echo "---" | tee -a /tmp/polybar.log
+polybar bar1 -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log &
+disown
+
+echo "Bars launched..."
+EOF
+
+chmod +x $HOME/.config/polybar/launch.sh
