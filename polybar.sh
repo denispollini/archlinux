96aaae627f091e1e56b2c2a093df0bcf50ffe3a1
Denis Pollini <denis.pollini.90@gmail.com>
2026-03-09 21:46:06 +0100

    edit script

--- polybar.sh
+++ polybar.sh
@@ -147,21 +147,21 @@ screenchange-reload = true
 pseudo-transparency = true
 EOF
 
-cat <<EOF > $HOME/.config/polybar/launch.sh
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch bar1
-echo "---" | tee -a /tmp/polybar.log
-polybar bar1 -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log &
-disown
-
-echo "Bars launched..."
-EOF
-
-chmod +x $HOME/.config/polybar/launch.sh
+#cat <<EOF > $HOME/.config/polybar/launch.sh
+##!/usr/bin/env bash
+#
+## Terminate already running bar instances
+## If all your bars have ipc enabled, you can use
+#polybar-msg cmd quit
+## Otherwise you can use the nuclear option:
+## killall -q polybar
+#
+## Launch bar1
+#echo "---" | tee -a /tmp/polybar.log
+#polybar bar1 -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log &
+#disown
+#
+#echo "Bars launched..."
+#EOF
+#
+#chmod +x $HOME/.config/polybar/launch.sh
